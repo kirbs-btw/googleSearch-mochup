6835502c367a95bae88df2c189e27ac44ed217eb
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-11-27 18:13:24 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+data = {
+  {
+    'link' : 'https://dasisteinlink.com',
+    'head' : 'Dasisteinlink',
+    'description' : 'hier steht ein haufen text, den man nur schwer lesen kann aber er saget irgendwas über die seite aus oder so keine ahnung man kann noch viel weiter schreiben aber das wäre nicht nötig ich finde es sehr wichtig immer alles zu leseen was hier steht da sind oft schreibfehler drinnen</p>',
+  },
+}
+
+function insertData(data){
+  for (var i = 0; i < data.length; i++) {
+    // do some insert
+    // i have no time today sorry
+  }
+}
