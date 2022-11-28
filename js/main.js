ff3fe20fd2fbf8a1c771176b8fc7f7cc19fdb48b
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-11-28 19:15:59 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-data = {
+var data = [
   {
     'link' : 'https://dasisteinlink.com',
     'head' : 'Dasisteinlink',
     'description' : 'hier steht ein haufen text, den man nur schwer lesen kann aber er saget irgendwas über die seite aus oder so keine ahnung man kann noch viel weiter schreiben aber das wäre nicht nötig ich finde es sehr wichtig immer alles zu leseen was hier steht da sind oft schreibfehler drinnen</p>',
   },
-}
+  {
+    'link' : 'https://dasisteinlink.com',
+    'head' : 'Dasisteinlink',
+    'description' : 'hier steht ein haufen text, den man nur schwer lesen kann aber er saget irgendwas über die seite aus oder so keine ahnung man kann noch viel weiter schreiben aber das wäre nicht nötig ich finde es sehr wichtig immer alles zu leseen was hier steht da sind oft schreibfehler drinnen</p>',
+  },
+  {
+    'link' : 'https://dasisteinlink.com',
+    'head' : 'Dasisteinlink',
+    'description' : 'hier steht ein haufen text, den man nur schwer lesen kann aber er saget irgendwas über die seite aus oder so keine ahnung man kann noch viel weiter schreiben aber das wäre nicht nötig ich finde es sehr wichtig immer alles zu leseen was hier steht da sind oft schreibfehler drinnen</p>',
+  },
+  {
+    'link' : 'https://dasisteinlink.com',
+    'head' : 'Dasisteinlink',
+    'description' : 'hier steht ein haufen text, den man nur schwer lesen kann aber er saget irgendwas über die seite aus oder so keine ahnung man kann noch viel weiter schreiben aber das wäre nicht nötig ich finde es sehr wichtig immer alles zu leseen was hier steht da sind oft schreibfehler drinnen</p>',
+  },
+];
 
 function insertData(data){
+  var items = "";
   for (var i = 0; i < data.length; i++) {
-    // do some insert
-    // i have no time today sorry
+    contentLine = `<div class="content-section"><p>${data[i].link}</p> <a href="#">${data[i].head}</a> <p>${data[i].description}</p><br></div>`;
+    items = items + contentLine;
   }
+  document.getElementById('left-section-content').innerHTML = items;
 }
+
+console.log("hello");
+
+insertData(data);
