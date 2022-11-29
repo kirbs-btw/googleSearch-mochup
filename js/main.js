68d2fd13205aa8f9ee386501f4cc2d238529883b
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-11-29 13:18:14 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var data = [
+var blockData = [
   {
     'link' : 'https://dasisteinlink.com',
     'head' : 'Dasisteinlink',
@@ -21,15 +21,21 @@ var data = [
   },
 ];
 
-function insertData(data){
+var sideData = {
+  'icon' : 'icon',
+  'description' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}
+
+function insertData(data, sideBlock){
   var items = "";
   for (var i = 0; i < data.length; i++) {
     contentLine = `<div class="content-section"><p>${data[i].link}</p> <a href="#">${data[i].head}</a> <p>${data[i].description}</p><br></div>`;
     items = items + contentLine;
   }
   document.getElementById('left-section-content').innerHTML = items;
+  document.getElementById('right-section-left-description-block').innerHTML = sideBlock.description;
 }
 
 console.log("hello");
 
-insertData(data);
+insertData(blockData, sideData);
